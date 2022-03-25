f364c347fdceeafcee64bf44f735c9f49bcc4233
Magnus <magnujo@ruc.dk>
2022-03-25 11:50:51 +0100

    exercises

--- src/com/company/installationtests/HelloBC.java
+++ src/com/company/installationtests/HelloBC.java
@@ -1,8 +1,13 @@
 package com.company.installationtests;
 
+import java.math.BigInteger;
+
 class HelloBC {
 
   public static void main(String args[]) {
     System.out.println("IntelliJ sees util.encoder.Hex");
+
+    System.out.println(Math.pow(10,5) % 91);
+    System.out.println(Math.pow(82,29) % 81);
   }
 }
--- src/com/company/installationtests/Main.java
+++ src/com/company/installationtests/Main.java
@@ -1,9 +1,38 @@
 package com.company.installationtests;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 public class Main {
 
-    public static void main(String[] args) {
-	 System.out.println("Hello world");
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException {
+        System.out.println(System.currentTimeMillis());
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "BC");
+        generator.initialize(3072);
+        KeyPair keyPair = generator.generateKeyPair();
+
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        System.out.println(System.currentTimeMillis());
+
+        System.out.println("Private exponent:");
+        System.out.println(rsaPrivateKey.getPrivateExponent());
+
+        System.out.println("Private mod: ");
+        System.out.println(rsaPrivateKey.getModulus());
+
+        System.out.println("Public exponent: ");
+        System.out.println(rsaPublicKey.getPublicExponent());
+
+        System.out.println("Public mod:");
+        System.out.println(rsaPublicKey.getModulus());
+
+
 
 
 
