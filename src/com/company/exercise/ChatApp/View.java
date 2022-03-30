86a8d189d31920827d79c999026fae9b3a6455ce
Magnus Johannsen <magnus_johannsen@hotmail.com>
2022-03-30 11:08:58 +0200

    gui done

--- src/com/company/exercise/ChatApp/View.java
+++ src/com/company/exercise/ChatApp/View.java
@@ -6,50 +6,49 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextFlow;
 
 public class View extends BorderPane {
 
     double windowHeight = 700;
     double windowWidth = 500;
+    double bpHeight = windowHeight*0.25;
+    double cpHeight = windowHeight*0.75;
 
 
     public View() {
-
-        VBox center = new VBox();
+        //Center:
+        //Chat pane (area to display chat messages)
         ScrollPane chatPane = new ScrollPane();
+        VBox chatBox = new VBox();
+        chatBox.setPrefHeight(cpHeight*0.5);
+        chatBox.setPrefWidth(chatPane.getPrefWidth());
+        chatPane.setFitToWidth(true);
+        chatPane.setPrefHeight(cpHeight);
+        chatPane.setContent(chatBox);
+
+        //Bottom pane (Text field and send button)
         HBox bottomPane = new HBox();
         Button sendButton = new Button("Send");
         TextArea textArea = new TextArea();
-
-
-        center.setPrefHeight(700);
-
-        double bpHeight = windowHeight*0.25;
-        double cpHeight = windowHeight*0.75;
-
-
-        bottomPane.setPrefHeight(bpHeight);
-        chatPane.setPrefHeight(cpHeight);
-
         textArea.setPrefHeight(bpHeight);
         textArea.setPrefWidth(windowWidth*0.60);
-
         sendButton.setPrefHeight(bpHeight);
         sendButton.setPrefWidth(windowWidth*0.30);
-
+        bottomPane.setPrefHeight(bpHeight);
         bottomPane.getChildren().addAll(textArea, sendButton);
 
+        VBox center = new VBox();
+        center.setPrefHeight(700);
         center.getChildren().addAll(chatPane,bottomPane);
+        setCenter(center);
 
+        //Left:
+        //Side pane
         ScrollPane sidePane = new ScrollPane();
         sidePane.setPrefWidth(250);
 
-
-
-        setCenter(center);
         setLeft(sidePane);
-
-
 
     }
 
